74f90c5be8e2448907272a835397fb5427c35a20
szosti14 <rafal.szostak2@gmail.com>
2022-06-28 14:43:19 +0200

    Delete QuizF1 directory

--- QuizF1/highscores.js
+++
@@ -1,6 +0,0 @@
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-const list = document.getElementById("highscores-list");
-
-list.innerHTML = highScores
-  .map((score) => `<li class="list-item">${score.name}: ${score.score}</li>`)
-  .join("");
